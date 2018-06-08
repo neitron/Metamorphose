957e862450e1259ddf9543b15e4d332e17d17d3a
Kirill Zubenko <kirill.zubenko.neitron@gmail.com>
2018-06-08 03:31:20 +0300

    Initial project commit

---
+++ Assets/Scripts/Metamorphose.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Metamorphose : MonoBehaviour 
+{
+
+
+	#region Unity properties
+
+	[SerializeField] private Mesh _mesh;
+	[SerializeField] private Mesh _meshTarget;
+	[SerializeField] private Transform _originalInstance;
+	[SerializeField, Range(0.001f, 1.0f)] private float _instanceScale;
+	[SerializeField] private float _physicsTimeDelay;
+
+	#endregion
+
+
+
+	private void Start () 
+	{
+		//spawn();
+		spawnPolies();
+	}
+	
+
+	private void spawnPolies()
+	{
+		List<Vector3> verticies = new List<Vector3>();
+		List<Vector3> normals = new List<Vector3>();
+		List<int> triangles = new List<int>();
+		List<Vector2> texCoords = new List<Vector2>();
+
+		_mesh.GetVertices(verticies);
+		_mesh.GetTriangles(triangles, 0);
+		_mesh.GetNormals(normals);
+		_mesh.GetUVs(0, texCoords);
+
+		MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
+
+		Mesh pMesh = new Mesh
+		{
+			vertices = new []
+			{
+				Vector3.zero,
+				Vector3.up, 
+				Vector3.left
+			}
+		};
+		pMesh.SetIndices(new[] { 0, 1, 2 }, MeshTopology.Triangles, 0);
+		pMesh.RecalculateNormals();
+
+		for(int t = 0; t < triangles.Count; t += 3)
+		{
+			GameObject p = new GameObject($"triangle_{t}");
+
+			Vector4[] verts =
+			{
+				transform.TransformPoint(verticies[triangles[t]]), 
+				transform.TransformPoint(verticies[triangles[t + 1]]), 
+				transform.TransformPoint(verticies[triangles[t + 2]])
+			};
+
+			Vector4[] norms =
+			{
+				transform.TransformVector(normals[triangles[t]]), 
+				transform.TransformVector(normals[triangles[t + 1]]), 
+				transform.TransformVector(normals[triangles[t + 2]])
+			};
+
+			Vector4[] uvs =
+			{
+				verticies[triangles[t]],
+				verticies[triangles[t + 1]],
+				verticies[triangles[t + 2]]
+			};
+
+
+			//ComputeBuffer vPoses = new ComputeBuffer(verts.Length, sizeof(float) * 3);
+			//vPoses.SetData(verts);
+
+			Transform instance = p.transform;
+			p.AddComponent<MeshFilter>().mesh = pMesh;
+			p.AddComponent<MeshRenderer>().sharedMaterial = _originalInstance.GetComponent<MeshRenderer>().sharedMaterial;
+
+			instance.SetParent(transform);
+
+			Color color = Color.Lerp(new Color(Random.value, Random.value, Random.value), Color.white, 0.7f); // just do it a bit white
+
+
+			// Draw the mesh with instancing.
+
+			materialPropertyBlock.SetMatrix("_VertexPositions", new Matrix4x4(verts[0], verts[1], verts[2], Vector4.zero));
+			materialPropertyBlock.SetMatrix("_VertexNormals", new Matrix4x4(norms[0], norms[1], norms[2], (norms[0] + norms[1] + norms[2]).normalized));
+			materialPropertyBlock.SetMatrix("_VertexUV", new Matrix4x4(uvs[0], uvs[1], uvs[2], Vector4.zero));
+			//vPoses.Release();
+
+			//materialPropertyBlock.SetColor("_Color", color);
+			//materialPropertyBlock.SetFloat("_Metallic", Random.value);
+			//materialPropertyBlock.SetFloat("_Glossiness", Random.value);
+			instance.GetComponent<MeshRenderer>().SetPropertyBlock(materialPropertyBlock);
+		}
+	}
+
+
+	private void spawn()
+	{
+		List<Vector3> verticiesList = new List<Vector3>();
+		_mesh.GetVertices(verticiesList);
+
+		HashSet<Vector3> verticies = new HashSet<Vector3>(verticiesList);
+		
+		Debug.Log(verticies.Count);
+
+		MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
+
+		foreach (Vector3 v in verticies)
+		{
+			Transform instance = Instantiate(_originalInstance);
+			instance.SetParent(transform);
+			instance.localScale = Vector3.one * _instanceScale;
+			instance.localPosition = v;
+
+			Color color = Color.Lerp(new Color(Random.value, Random.value, Random.value), Color.white, 0.7f); // just do it a bit white
+
+			materialPropertyBlock.SetColor("_Color", color);
+			//materialPropertyBlock.SetFloat("_Metallic", Random.value);
+			//materialPropertyBlock.SetFloat("_Glossiness", Random.value);
+			instance.GetComponent<MeshRenderer>().SetPropertyBlock(materialPropertyBlock);
+
+			instance.GetComponent<Rigidbody>().isKinematic = true;
+		}
+
+		StartCoroutine(runPhysicsLate());
+	}
+
+
+	private IEnumerator runPhysicsLate()
+	{
+		yield return new WaitForSeconds(_physicsTimeDelay);
+
+		foreach (Transform child in transform)
+		{
+			child.GetComponent<Rigidbody>().isKinematic = false;
+		}
+	}
+
+
+}
---
+++ Assets/Shaders/Sphere.shader
@@ -0,0 +1,91 @@
+﻿// Upgrade NOTE: upgraded instancing buffer 'Props' to new syntax.
+
+Shader "CS/GpuInstancing" 
+{
+
+	Properties 
+	{
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+	}
+
+	SubShader 
+	{
+		Tags 
+		{ 
+			"RenderType" = "Opaque" 
+		}
+
+		LOD 200
+		Cull Off
+
+		CGPROGRAM
+
+			// Physically based Standard lighting model, and enable shadows on all light types
+			#pragma surface surf Standard vertex:vert fullforwardshadows
+			#pragma instancing_options assumeuniformscaling
+
+			// Use shader model 3.0 target, to get nicer looking lighting
+			#pragma target 3.0
+
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float3 normal : NORMAL;
+				float4 tangent : TANGENT;
+				float4 texcoord : TEXCOORD0;
+				float4 texcoord1 : TEXCOORD1;
+				float4 texcoord2 : TEXCOORD2;
+				uint vid : SV_VertexID;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+
+
+			sampler2D _MainTex;
+			
+			struct Input 
+			{
+				float2 uv_MainTex;
+			};
+
+			UNITY_INSTANCING_BUFFER_START(Props)
+				UNITY_DEFINE_INSTANCED_PROP(fixed4, _Color)
+				UNITY_DEFINE_INSTANCED_PROP(half, _Glossiness)
+				UNITY_DEFINE_INSTANCED_PROP(half, _Metallic)
+				UNITY_DEFINE_INSTANCED_PROP(float4x4, _VertexPositions)
+				UNITY_DEFINE_INSTANCED_PROP(float4x4, _VertexNormals)
+				UNITY_DEFINE_INSTANCED_PROP(float4x4, _VertexUV)
+			UNITY_INSTANCING_BUFFER_END(Props)
+
+			void vert(inout appdata v)
+			{
+				float4x4 poses = UNITY_ACCESS_INSTANCED_PROP(Props, _VertexPositions);
+				v.vertex.xyz = float3 ( poses[0][v.vid], poses[1][v.vid], poses[2][v.vid] );
+
+				float4x4 norms = UNITY_ACCESS_INSTANCED_PROP(Props, _VertexNormals);
+				v.normal.xyz = float3 ( norms[0][v.vid], norms[1][v.vid], norms[2][v.vid] );
+
+				v.vertex.xyz = v.vertex.xyz + 0.01 * v.normal.xyz + ( saturate( sin(_Time.z + v.vertex.y) * 0.5 ) ) * float3 ( norms[0][3], norms[1][3], norms[2][3] );
+			
+				float4x4 uvs = UNITY_ACCESS_INSTANCED_PROP(Props, _VertexUV);
+				v.texcoord.xy =  float2 ( uvs[0][v.vid], uvs[1][v.vid] );
+			}
+
+
+			void surf (Input IN, inout SurfaceOutputStandard o)
+			{
+				fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * UNITY_ACCESS_INSTANCED_PROP(Props, _Color);
+				o.Albedo = c.rgb;
+				
+				o.Metallic = UNITY_ACCESS_INSTANCED_PROP(Props, _Metallic);;
+				o.Smoothness = UNITY_ACCESS_INSTANCED_PROP(Props, _Glossiness);;
+				o.Alpha = c.a;
+			}
+		
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
